7b29c90cfb12aeffc0da8eedd873a397ed2c1fb6
sado98 <sado9898kz@gmail.com>
2020-03-19 15:10:14 +0600

    Head first kotlin: work-1

---
+++ app/src/main/java/com/head/first/myfirstapp/App.kt
@@ -0,0 +1,8 @@
+package com.head.first.myfirstapp
+
+fun main() {
+    val x = 3
+    val y = 1
+    println(if (x > y) "x больше чем y" else "x не больше чем y")
+    println("Эта линия работать несмотря ни на что")
+}
